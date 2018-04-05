1f70947fb7842c4c03fddf07814f21a5afed7b0c
Sandeep Swaminathan <sandeepswaminathan@gmail.com>
2018-04-05 08:33:07 -0400

    ToDoList project

---
+++ todolist/src/main/java/com/spinmaster/todolist/repository/TodoRepository.java
@@ -0,0 +1,10 @@
+package com.spinmaster.todolist.repository;
+
+import com.spinmaster.todolist.model.Todo;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface TodoRepository extends MongoRepository<Todo, String> {
+
+}
